32e1edbf2305794941170350264ca9b2050da6e6
Maryam Ibikunle <ibikunlemaryamibidoyin@gmail.com>
2025-10-19 20:36:08 +0100

    Fully functional

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 const welcomePage = document.getElementById("welcome")
 const gamePage = document.getElementById('game-page')
+const lastPage = document.getElementById("last-page")
 const question = document.getElementById('question')
 const optionList = document.getElementById('options')
-const options = optionList.querySelectorAll("p")
+const options = optionList.querySelectorAll("label")
+const radios = document.querySelectorAll('input[name="option"')
 const boolButton = document.getElementById("bool")
 const startButton = document.getElementById("start")
 const nextButton = document.querySelectorAll('.next')
+const replayButton = document.getElementById("replay")
 const time = document.getElementById('time')
 gamePage.style.display = 'none'
+let score = 0;
 
 startButton.addEventListener('click', asyncFunct)
+replayButton.addEventListener('click', ()=>{
+    lastPage.style.display = "none"
+    score = 0;
+    document.getElementById('number').textContent = `Question 1`
+    asyncFunct()
+})
 
 async function asyncFunct(){
     welcomePage.style.display = "none"
     boolButton.style.display = "none"
     optionList.style.display = "none"
     gamePage.style.display = "flex"
-    await fetch('https://opentdb.com/api.php?amount=5')
+    await fetch('https://opentdb.com/api.php?amount=10')
     .then(response => response.json())
     .then(data => {
-        const [a,b,c,d,e] = data.results
-        const allQuestions = [a,b,c,d,e]
+        const [a,b,c,d,e,f,g,h,i,j] = data.results
+        const allQuestions = [a,b,c,d,e,f,g,h,i,j]
         // for (let i=0; i<5; i++){
         //     showNextPage(allQuestions[i])
         // } 
@@ -31,9 +41,8 @@ async function asyncFunct(){
     .catch(err => console.log("ERROR: ", err))}
 
 function showNextPage(list){
-    let i =0
+    let i = 0
     displayQuiz(list[i])
-    i++
     let counter;
     let timeLeft = 20;
     counter = setInterval(()=>{
@@ -41,36 +50,40 @@ function showNextPage(list){
         time.innerHTML = `00:${String(timeLeft).padStart(2,0)}`
         if (timeLeft <=5)
             time.style.color = "red"
-        if (timeLeft == 0)
+        if (timeLeft == 0){
             clearInterval(counter)
-            //endgame()
+            displayScore()}
     },1000)
     
     
     nextButton.forEach((btn)=>{btn.addEventListener('click',()=>{
+        console.log("index"+i)
         clearInterval(counter)
+        document.getElementById('number').textContent = `Question ${i+2}`
+
+        i++
+        if (i==10)displayScore()
         if (i<list.length){
             displayQuiz(list[i])
-            i++
+            
             let timeLeft = 20;
+            time.innerText = "00:20"
             counter = setInterval(()=>{
                 timeLeft--
-                time.innerHTML = `00:${String(timeLeft).padStart(2,0)}`
+                time.innerText = `00:${String(timeLeft).padStart(2,0)}`
                 if (timeLeft <=5)
                     time.style.color = "red"
                 if (timeLeft == 0)
                     clearInterval(counter)
-                    //endgame()
             },1000)
-            
         }
+        
     })})}
 
-    // }else{
-    //     showResult()
-
-
+    
 function displayQuiz(object){
+    
+    currentQuestion = object;
     question.innerText = object.question;
     if (object.type == "boolean"){
         optionList.style.display = "none"
@@ -78,12 +91,9 @@ function displayQuiz(object){
     }else if (object.type == "multiple"){
         boolButton.style.display = "none"
         multiple(object)
-    }
-    
-    
-        
-    // clearInterval(counter)
+    }   
 }
+
 
 function boolean(object){
     boolButton.style.display = "block"
@@ -100,10 +110,38 @@ function multiple(object){
         myOptions.push("Bonus")
         myOptions.push("Bonus")
     }
-    myOptions.push(object.correct_answer)
+    const index = Math.floor(Math.random()*4)
+    console.log(index)
+    myOptions.splice(index, 0,(object.correct_answer))
     myOptions.forEach((option, index) => {
         if (options[index])
             options[index].innerText = option
     });
 }
+optionList.addEventListener('change', (e) => {
+    const radio = e.target;
+    if(radio.name !== "option") return;
+    const label = document.querySelector(`label[for="${radio.id}"]`);
+    if(!label) return;
+    const selected = label.textContent;
+    if(selected === currentQuestion.correct_answer){
+        score++;
+    }else{
+        document.getElementById("correct").style.color = "red"
+        document.getElementById("correct").style.fontWeight = "bold"
+        document.getElementById("correct").innerText = currentQuestion.correct_answer
+    }
+    console.log("score:", score);
+    console.log("Selected:", selected);
+    console.log("Correct answer:", currentQuestion.correct_answer);
+    
+});
 
+function displayScore(){
+    welcomePage.style.display = "none"
+    gamePage.style.display = "none"
+    lastPage.style.display = "flex"
+    lastPage.style.flexDirection = "column"
+    document.getElementById("score").textContent = `${score}/10`
+
+}
