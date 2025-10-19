b34151073307d7e138062398e84bac0b5273cd28
Maryam Ibikunle <ibikunlemaryamibidoyin@gmail.com>
2025-10-19 13:14:34 +0100

    Displays options or bools as necessary

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ const gamePage = document.getElementById('game-page')
 const question = document.getElementById('question')
 const optionList = document.getElementById('options')
 const options = optionList.querySelectorAll("p")
+const boolButton = document.getElementById("bool")
 gamePage.style.display = 'none'
 const startButton = document.getElementById("start")
 startButton.addEventListener('click', asyncFunct)
 
 async function asyncFunct(){
     welcomePage.style.display = "none"
+    boolButton.style.display = "none"
+    optionList.style.display = "none"
     gamePage.style.display = "flex"
     await fetch('https://opentdb.com/api.php?amount=5')
     .then(response => response.json())
@@ -17,13 +20,14 @@ async function asyncFunct(){
         const allQuestions = [a,b,c,d,e]
         for (let i=0; i<5; i++){
             showNextPage(allQuestions[i])
-        }
-        
+        } 
         console.log(a.question)
     }
     )
     .catch(err => console.log("ERROR: ", err))}
+
 function showNextPage(object){
+    
         displayQuiz(object)
     // }else{
     //     showResult()
@@ -35,20 +39,27 @@ function displayQuiz(object){
     if (object.type == "boolean"){
         boolean(object)
     }else if (object.type == "multiple"){
+        boolButton.style.display = "none"
         multiple(object)
     }
 }
 
 function boolean(object){
-    document.getElementById('options').display = "none"
+    optionList.style.display = "none"
+    boolButton.style.display = "block"
     if (object.correct_answer == "True"){
         console.log("True")
     }
 }
 
 function multiple(object){
-    document.getElementById('bool').display = "none"
+    optionList.style.display = "block"
     const myOptions = object.incorrect_answers
+    if (myOptions[0] == Array){
+        myOptions.push("Bonus")
+        myOptions.push("Bonus")
+        myOptions.push("Bonus")
+    }
     myOptions.push(object.correct_answer)
     myOptions.forEach((option, index) => {
         if (options[index])
